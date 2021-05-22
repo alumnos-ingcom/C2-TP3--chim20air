2d6331672c6faf779a53348e040facdbc0f2656f
Adrian Evaraldo <adrievar@gmail.com>
2021-05-22 17:37:44 -0300

    int listo, vamos por rango
    todavia estoy trabajando en el tema de la deteccion del rango de entrada. Estoy trabajando bajo el supuesto de que no le paso los valores inferior y superior del rango de entrada a la funcion ingreso_entero_reintento. Cosa que no veo como correcta @martinvilu

--- tp4ej1.py
+++ tp4ej1.py
@@ -9,31 +9,44 @@ class IngresoIncorrecto(Exception):
     pass 
 
 def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
-    """Manejo de error del tipo de entrada"""
-    ingreso = input(mensaje + ">>> ")
+    """Manejo de error del tipo de entrada""" 
+    if(mensaje == "Pibe te estoy pidiendo un entero"):
+        try:
+            print("Me voy a poner como el de Jurassic Park")
+            print(f"Te quedan: {cantidad_reintentos} reintentos""")
+            entero = int(input(mensaje + ">>> "))
+        
+        except ValueError as err:
+            if cantidad_reintentos == 0:
+                raise IngresoIncorrecto("Volve a primaria pibe") from err
+            else:
+                entero = ingreso_entero_reintento(mensaje,
+                        cantidad_reintentos=cantidad_reintentos-1)
+        return entero
+    
+    elif(mensaje == "Atenente al rango"):
+        print("hello")
+
+def IngresoEntero(mensaje):
     try:
-        entero = int(ingreso)
-    except ValueError as err:
-        print(cantidad_reintentos)
-        if cantidad_reintentos == 0:
-            raise IngresoIncorrecto("Volve a primaria pibe") from err
-        else:
-            ingreso_entero_reintento(mensaje, cantidad_reintentos=cantidad_reintentos-1)
+        entero = int(input(mensaje + ">>> "))
+    except ValueError:
+        entero = ingreso_entero_reintento(f"Pibe te estoy pidiendo un entero")        
     return entero
 
 def ingreso_entero_restringido(mensaje, valor_minimo=0, valor_maximo=10):
-    if not valor_minimo < 
-
-def IngresoEntero(mensaje):
-    global valor_minimo, valor_maximo
-    ingreso = input(mensaje + ">>> ")
+    """Funcion para restringir el rango de ingreso"""
     try:
-        entero = int(ingreso)
-    except ValueError:
-        ingreso_entero_reintento("Pibe un entero te estoy pidiendo")
-    else:
-        check = ingreso_entero_restringido("Verificacion de rango") 
-        return entero
+        valor = IngresoEntero(mensaje)
+        if valor_minimo > valor or valor > valor_maximo:
+            raise ValueError
+    except ValueError as err:
+        print(f"No pibe, el entero tiene que estar entre {valor_minimo} y {valor_maximo}")
+        valor = ingreso_entero_restringido("Atenente al rango")
+
+    return valor
+
 
 if __name__ == "__main__":
-    print(IngresoEntero("mandale mecha"))
+    print(IngresoEntero("mandale un entero"))
+    print(ingreso_entero_restringido("ahora probamos el rango"))
